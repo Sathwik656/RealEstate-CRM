61e3003cf9970b28d108c94dcf3a1abf4f087ece
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2026-06-10 23:12:29 +0530

    Added android app build

---
+++ frontend/android/app/src/main/java/com/veenu/crm/MainActivity.java
@@ -0,0 +1,5 @@
+package com.veenu.crm;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.veenu.crm',
+  appName: 'veenu-crm-client',
+  webDir: 'dist'
+};
+
+export default config;
